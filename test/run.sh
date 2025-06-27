2efce9332eee0f1a91a72d44aa13fc930df2a86f
Micha de Vries <micha@devrie.sh>
2025-06-27 10:17:06 +0200

    Include more examples

--- test/run.sh
+++ test/run.sh
@@ -15,3 +15,7 @@ echo " "
 ./surrealism-runtime run --name can_drive --arg 18 out.wasm
 echo " "
 ./surrealism-runtime run --name can_drive --arg 17 out.wasm
+echo " "
+./surrealism-runtime run --name create_user --arg '["Micha", 21]' --arg true out.wasm
+echo " "
+./surrealism-runtime run --arg 18 out.wasm
